4c253edc65a4041a85158bdd9103cbe9ea904bdb
mercedes sandu <mxrcedes@gmail.com>
2022-10-23 23:21:19 -0500

    soda machine wip

--- Assets/Scripts/Soda Machine/SodaMachineCanvas.cs
+++ Assets/Scripts/Soda Machine/SodaMachineCanvas.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class SodaMachineCanvas : MonoBehaviour
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField] private GameObject cup;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField] private GameObject fillLine;
     
+    /// <summary>
+    /// 
+    /// </summary>
+    public void CloseCanvas()
+    {
+        gameObject.SetActive(false);
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public void CompleteSodaOrder()
+    {
+        
+    }
 }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Papas What Is Going On Eria
 
+Candy Beans font: https://www.dafont.com/candy-beans.font
 Receipt font: https://www.dafont.com/fake-receipt.font
 Pixel star: https://soulofkiran.itch.io/pixel-art-animated-star
