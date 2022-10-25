4b751def824158b8ce685e415bb619a01569d010
mercedes sandu <mxrcedes@gmail.com>
2022-10-25 01:01:11 -0500

    scoring supposedly done

--- Assets/Scripts/Scoring/ScoringSystem.cs
+++ Assets/Scripts/Scoring/ScoringSystem.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Scripts;
 using TMPro;
 using UnityEngine;
@@ -53,12 +54,14 @@ public class ScoringSystem : MonoBehaviour
     }
 
     /// <summary>
-    /// Adds the specified amount to the current score.
+    /// Adds the specified amount to the current score. If this results in a negative score, the score is set to 0.
     /// </summary>
     /// <param name="amount">The amount to add.</param>
     private void ChangeScore(int amount)
     {
         score += amount;
+        
+        if (score < 0) score = 0;
     }
     
     /// <summary>
@@ -81,6 +84,20 @@ public class ScoringSystem : MonoBehaviour
     {
         int decrementValue = 0;
         
+        // Check if the correct types of ingredients were used.
+        decrementValue = necessaryIngredients.Keys.Where(type => usedIngredients
+            .FindIndex(x => x.Item2 == type) == -1)
+            .Aggregate(0, (current, type) => current - 10);
+
+        // Check if the correct ingredients were used.
+        decrementValue = usedIngredients.Where(ingredient => necessaryIngredients.Keys
+            .Contains(ingredient.Item2) && !ReferenceEquals(necessaryIngredients[ingredient.Item2], ingredient.Item1))
+            .Aggregate(decrementValue, (current, ingredient) => current - 10);
+
+        // Check if the ingredients were placed at the correct positions.
+        decrementValue = xDistances.Aggregate(decrementValue, (current, distance) => current - (int)distance);
+
+        // Change the score.
         GameEvent.ChangeScore(decrementValue);
     }
     
@@ -94,6 +111,16 @@ public class ScoringSystem : MonoBehaviour
     {
         int decrementValue = 0;
         
+        // Check if the correct soda was used.
+        if (!ReferenceEquals(necessarySoda, usedSoda))
+        {
+            decrementValue -= 10;
+        }
+        
+        // Check if the soda was filled to the correct height.
+        decrementValue -= (int) heightDifference;
+        
+        // Change the score.
         GameEvent.ChangeScore(decrementValue);
     }
 
