0e194cc51cece8551b244c7d37560ec0e47f77bb
mercedes sandu <mxrcedes@gmail.com>
2022-10-27 19:31:34 -0500

    change done buttons

--- Assets/Scripts/Scoring/ScoringSystem.cs
+++ Assets/Scripts/Scoring/ScoringSystem.cs
@@ -61,8 +61,9 @@ public class ScoringSystem : MonoBehaviour
     private void ChangeScore(int amount)
     {
         score += amount;
-
+        // todo: remove this
         if (score < 0) score = 0;
+        Debug.Log("score: " + score);
     }
 
     /// <summary>
@@ -83,6 +84,7 @@ public class ScoringSystem : MonoBehaviour
         // todo: debug
         int requiredCookTime = Order.Instance.GetCookTime();
         int cookedTime = (int)(amount / 90) + 2;
+        // todo: remove this
         Debug.Log("amount: " + amount + " cookedTime: " + cookedTime + " requiredCookTime: " + requiredCookTime);
         GameEvent.ChangeScore(Math.Abs(cookedTime - requiredCookTime) * -5);
     }
